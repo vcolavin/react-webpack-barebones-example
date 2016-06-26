47af8fc2a49eb6b55abe9b456ca1dbb8101eac16
vcolavin <vcolavin@gmail.com>
2016-06-26 12:27:22 -0700

    make react render into dedicated component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 var React = require('react');
+var ReactDOM = require('react-dom')
 
 var App = require('./components/App');
 
 if(typeof document !== "undefined")
 {
-    React.render(<App />, document.body);
+    ReactDOM.render(<App />, document.getElementById('app')
+);
 }
